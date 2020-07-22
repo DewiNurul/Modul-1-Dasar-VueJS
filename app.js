e2b87d17f381e6d8ebc7dc0ef2230ccf58e08056
DewiNurul <dewi_nurul_27rpl@student.smktelkom-mlg.sch.id>
2020-07-22 23:19:12 +0700

    modul 1

---
+++ app.js
@@ -0,0 +1,14 @@
+ var app = new Vue({ 
+    el : '#app',
+    data : {
+        'message' : 'Hello World'
+    }
+});
+// New Vue => membuat inisialisasi vue objek
+// vue objek untuk mengontrol elemen div #app, misal menampilkan teks
+// el => properti pertama yg nilai / value nya adalah id dari elemen yg mjd target dikontrol, yaitu app
+// di dalam div #app ada elemen h1 Hello World yg akan ditampilkan sbg h1 melalui properti
+// property data merupakan object sehingga juga memiliki property dan value
+// 'message' => property dengan 'hello world' => value
+
+   
